0f4cc03db53c705c7f776a29d6b455bb5ffd8813
Amit Prasad <mail@amitprasad.dev>
2024-02-20 17:04:44 -0600

    Quark support w/o compat contract

--- packages/unstake/src/adapter/quark.rs
+++ packages/unstake/src/adapter/quark.rs
@@ -1,27 +1,107 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, Coin, CosmosMsg, CustomQuery, Decimal, QuerierWrapper, StdResult};
+use cosmwasm_std::{
+    wasm_execute, Addr, Coin, CosmosMsg, CustomQuery, Decimal, QuerierWrapper, StdResult,
+};
 
-use super::{eris::Eris, interface::Unstake};
+use super::interface::Unstake;
 #[cw_serde]
-pub struct Quark(Eris);
+pub struct Quark {
+    liq: Addr,
+    hub: Addr,
+}
+
+impl Quark {
+    pub fn new(liq: Addr, hub: Addr) -> Self {
+        Self { liq, hub }
+    }
+}
 
 // The address provided is the bow-compat contract by Quark, so it uses the same interfaces as Eris
 impl Unstake for Quark {
     fn redemption_rate<T: CustomQuery>(&self, querier: QuerierWrapper<T>) -> StdResult<Decimal> {
-        self.0.redemption_rate(querier)
+        let state: liquifier::StateResponse =
+            querier.query_wasm_smart(&self.liq, &liquifier::QueryMsg::State {})?;
+        Ok(state.rate)
     }
 
     fn unbond_start<T>(&self, funds: Coin) -> CosmosMsg<T> {
-        self.0.unbond_start(funds)
+        wasm_execute(
+            &self.liq,
+            &liquifier::ExecuteMsg::Unwrap(liquifier::UnwrapMsg {
+                target: Some(liquifier::UnwrapTarget::Underlying),
+                callback: None,
+            }),
+            vec![funds],
+        )
+        .unwrap()
+        .into()
     }
 
     fn unbond_end<T>(&self) -> CosmosMsg<T> {
-        self.0.unbond_end()
+        wasm_execute(
+            &self.hub,
+            &hub::ExecuteMsg::Withdraw(hub::WithdrawMsg {
+                recipient: None,
+                callback: None,
+            }),
+            vec![],
+        )
+        .unwrap()
+        .into()
     }
 }
 
-impl From<Addr> for Quark {
-    fn from(value: Addr) -> Self {
-        Self(Eris::from(value))
+mod liquifier {
+    use cosmwasm_schema::{cw_serde, QueryResponses};
+    use cosmwasm_std::{Decimal, Uint128};
+    use kujira::CallbackData;
+    #[cw_serde]
+    #[derive(QueryResponses)]
+    pub enum QueryMsg {
+        #[returns(StateResponse)]
+        State {},
+    }
+
+    #[cw_serde]
+    pub struct StateResponse {
+        pub supply: Uint128,
+        pub rate: Decimal,
+    }
+
+    #[cw_serde]
+    pub enum ExecuteMsg {
+        /// Unwrap LSD tokens into either hub, unifier, or underlying tokens.
+        /// If unwrapping into underlying, this will begin the unstake process on the hub contract.
+        /// Unstake progress is tracked by the hub contract.
+        Unwrap(UnwrapMsg),
+    }
+
+    #[cw_serde]
+    pub enum UnwrapTarget {
+        Hub,
+        Underlying,
+    }
+    #[cw_serde]
+    pub struct UnwrapMsg {
+        pub target: Option<UnwrapTarget>,
+        pub callback: Option<CallbackData>,
+    }
+}
+
+mod hub {
+    use cosmwasm_schema::cw_serde;
+    use cosmwasm_std::Addr;
+    use kujira::CallbackData;
+
+    #[cw_serde]
+    pub enum ExecuteMsg {
+        /// Withdraw unbonded tokens after the unbond period.
+        Withdraw(WithdrawMsg),
+    }
+
+    #[cw_serde]
+    pub struct WithdrawMsg {
+        pub recipient: Option<Addr>,
+        pub callback: Option<CallbackData>,
     }
 }
